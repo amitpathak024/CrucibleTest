8f2b64c52137b597b9521889bad505cb521fe4f0
Amit Pathak <amitpathak024@gmail.com>
2016-07-04 14:02:32 +0530

    First Commit

---
+++ TestingApplication/TestingApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestingApplication.Startup))]
+namespace TestingApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
